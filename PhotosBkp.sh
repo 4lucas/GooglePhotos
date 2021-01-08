c65d313a4621de6d57d2e2f63ffe7245110b739f
iLucas Bernardes <ilucasbernardes@me.com>
2021-01-08 13:59:26 -0300

    Update PhotosBkp.sh

--- PhotosBkp.sh
+++ PhotosBkp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#./adb.sh 
 bg adb shell &
-adb push "/home/infra/Dropbox/Camera Uploads/"* /sdcard/ExternalBackup
-rm -rf "/home/infra/Dropbox/Camera Uploads/"*
+adb push "/home/user/Dropbox/Camera Uploads/"* /sdcard/ExternalBackup
+rm -rf "/home/user/Dropbox/Camera Uploads/"*
