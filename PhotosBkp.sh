1522874acbd697ffe79e6bf0d998879fb9190cd6
iLucas Bernardes <ilucasbernardes@me.com>
2021-02-15 13:35:13 -0300

    Update PhotosBkp.sh

--- PhotosBkp.sh
+++ PhotosBkp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 bg adb shell &
 #Photos and videos from Dropbox
-adb push "/home/infra/Dropbox/Camera Uploads/"* /sdcard/ExternalBackup
+adb push "/home/user/Dropbox/Camera Uploads/"* /sdcard/ExternalBackup
 
 #Photos and videos from the other device
-adb push "/media/infra/NIKON/DCIM/100NIKON/"* /sdcard/ExternalBackup
+adb push "/media/user/NIKON/DCIM/100NIKON/"* /sdcard/ExternalBackup
 
 while true; do
     read -p "Do you wish delete local archives?" yn
