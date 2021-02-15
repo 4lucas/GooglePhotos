14b174b6d84bb5aa1e01fdfc34e9d21a89d3d7a4
iLucas Bernardes <ilucasbernardes@me.com>
2021-02-15 13:30:00 -0300

    Option to delete files already uploaded
    It becomes possible to free up local storage, according to the user's option

--- PhotosBkp.sh
+++ PhotosBkp.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 bg adb shell &
-adb push "/home/user/Dropbox/Camera Uploads/"* /sdcard/ExternalBackup
-rm -rf "/home/user/Dropbox/Camera Uploads/"*
+#Photos and videos from Dropbox
+adb push "/home/infra/Dropbox/Camera Uploads/"* /sdcard/ExternalBackup
+
+#Photos and videos from the other device
+adb push "/media/infra/NIKON/DCIM/100NIKON/"* /sdcard/ExternalBackup
+
+while true; do
+    read -p "Do you wish delete local archives?" yn
+    case $yn in
+        [Yy]* ) rm -rf "/home/infra/Dropbox/Camera Uploads/"*; rm -rf "/media/infra/NIKON/DCIM/100NIKON/"*; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
