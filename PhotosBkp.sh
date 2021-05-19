546134b6d326a0730f89498f8fcb372662f28580
iLucas Bernardes <ilucasbernardes@me.com>
2021-05-19 16:22:54 -0300

    Update PhotosBkp.sh
    Move contents to /home

--- PhotosBkp.sh
+++ PhotosBkp.sh
@@ -6,20 +6,20 @@
 #forked from andreafabrizi/Dropbox-Uploader
 #
 #Comment the next line if you don't use Dropbox download using "Dropbox-Uploader" (API)
-"/home/user/Área de Trabalho/Google Photos/Dropbox-Uploader/"./dropbox_uploader.sh download "/Camera Uploads" "/home/user/Dropbox/"
+"/home/pi/GooglePhotos/Dropbox-Uploader/"./dropbox_uploader.sh download "/Camera Uploads" "/home/pi/Dropbox/"
 
 bg adb shell &
 #Photos and videos from Dropbox
-adb push "/home/user/Dropbox/Camera Uploads/"* /sdcard/ExternalBackup
+adb push "/home/pi/Dropbox/Camera Uploads/"* /sdcard/ExternalBackup
 
 #Photos and videos from the other device
-adb push "/media/user/NIKON/DCIM/100NIKON/"* /sdcard/ExternalBackup
+adb push "/media/pi/NIKON/DCIM/100NIKON/"* /sdcard/ExternalBackup
 
 #Exclude local and Dropbox archives
 while true; do
     read -p "Do you wish delete local and online archives?" yn
     case $yn in
-        [Yy]* ) rm -rf "/home/user/Dropbox/Camera Uploads/"*; rm -rf "/media/user/NIKON/DCIM/100NIKON/"*; "/home/user/Área de Trabalho/Google Photos/Dropbox-Uploader/"./dropbox_uploader.sh delete "/Camera Uploads"; "/home/user/Área de Trabalho/Google Photos/Dropbox-Uploader/"./dropbox_uploader.sh mkdir "/Camera Uploads";  break;;
+        [Yy]* ) rm -rf "/home/pi/Dropbox/Camera Uploads/"*; rm -rf "/media/pi/NIKON/DCIM/100NIKON/"*; "/home/pi/GooglePhotos/Dropbox-Uploader/"./dropbox_uploader.sh delete "/Camera Uploads"; "/home/pi/GooglePhotos/Dropbox-Uploader/"./dropbox_uploader.sh mkdir "/Camera Uploads";  break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
